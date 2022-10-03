53302a2df737e002b7b09e4d78b04f8b3116ff2d
Hung Hoang <Hunghoang159bmt@gmail.com>
2022-10-03 15:29:06 +0700

    first commit

---
+++ data/songPlaylists.js
@@ -0,0 +1,463 @@
+
+var listSong = [
+    [
+        {
+            name: "Xin Má Rước Dâu",
+            singer: ["Diệu Kiên"],
+            path: "./assets/music/listSong1/song1.mp3",
+            image: "./assets/img/music/listSong1/song1.jpg"
+        },
+        {
+            name: "Hạnh Phúc Đó Em Không Có Remix",
+            singer: ["Lương Minh Trang"],
+            path: "./assets/music/listSong1/song2.mp3",
+            image: "./assets/img/music/listSong1/song2.jpg"
+        },
+        {
+            name: "Cheer Up",
+            singer: ["Hong Jin Young"],
+            path: "./assets/music/listSong1/song3.mp3",
+            image: "./assets/img/music/listSong1/song3.jpg"
+        },
+        {
+            name: "Thương Thầm",
+            singer: ["Hoài Bảo"],
+            path: "./assets/music/listSong1/song4.mp3",
+            image: "./assets/img/music/listSong1/song4.jpg"
+        },
+        {
+            name: "Chốn Quê Thanh Bình",
+            singer: ["Dimz"],
+            path: "./assets/music/listSong1/song5.mp3",
+            image: "./assets/img/music/listSong1/song5.jpg"
+        },
+        {
+            name: "Có không giữ mất đừng tìm",
+            singer: ["Nguyễn Hằng"],
+            path: "./assets/music/listSong1/song6.mp3",
+            image: "./assets/img/music/listSong1/song6.jpg"
+        },
+        {
+            name: "Mashup",
+            singer: ["YLing vs Rum7"],
+            path: "./assets/music/listSong1/song7.mp3",
+            image: "./assets/img/music/listSong1/song7.jpg"
+        },
+        {
+            name: "Lời Anh Muốn Nói",
+            singer: ["Chu Duyên"],
+            path: "./assets/music/listSong1/song8.mp3",
+            image: "./assets/img/music/listSong1/song8.jpg"
+        },
+        {
+            name: "Váy Cưới",
+            singer: ["Trung Tự"],
+            path: "./assets/music/listSong1/song9.mp3",
+            image: "./assets/img/music/listSong1/song9.jpg"
+        },
+        {
+            name: "Kiệu Hoa (RIN Music Remix)",
+            singer: ["Bìn"],
+            path: "./assets/music/listSong1/song10.mp3",
+            image: "./assets/img/music/listSong1/song10.jpg"
+        },
+        {
+            name: "Mặt Mộc (RIN Music Remix)",
+            singer: ["Phạm Nguyên Ngọc x VAnh"],
+            path: "./assets/music/listSong1/song11.mp3",
+            image: "./assets/img/music/listSong1/song11.jpg"
+        },
+        {
+            name: "Ví Dầu Đưa Dâu (Cover)",
+            singer: ["Changmie x Tiến Tới"],
+            path: "./assets/music/listSong1/song12.mp3",
+            image: "./assets/img/music/listSong1/song12.jpg"
+        },
+        {
+            name: "Thuyền Quyên Cover",
+            singer: ["Chuppi"],
+            path: "./assets/music/listSong1/song13.mp3",
+            image: "./assets/img/music/listSong1/song13.jpg"
+        },
+        {
+            name: "Duyên Duyên Số Số (RIN Music Remix)",
+            singer: ["Du Uyên"],
+            path: "./assets/music/listSong1/song14.mp3",
+            image: "./assets/img/music/listSong1/song14.jpg"
+        },
+        {
+            name: "Nặng Tình Hay Nhẹ Lòng (WRC REMIX)",
+            singer: ["Tống Gia Vỹ"],
+            path: "./assets/music/listSong1/song15.mp3",
+            image: "./assets/img/music/listSong1/song15.jpg"
+        },
+        {
+            name: "Quên Người Đã Quá Yêu (Orinn Remix)",
+            singer: ["Hà Duy Thái"],
+            path: "./assets/music/listSong1/song16.mp3",
+            image: "./assets/img/music/listSong1/song16.jpg"
+        },
+        {
+            name: "Cùng Anh",
+            singer: ["Ngọc Dolil", "Hagi", "STee"],
+            path: "./assets/music/listSong1/song17.mp3",
+            image: "./assets/img/music/listSong1/song17.jpg"
+        },
+        {
+            name: "Lời Anh Chưa Thể Nói (Orinn Remix)",
+            singer: ["Chubby"],
+            path: "./assets/music/listSong1/song18.mp3",
+            image: "./assets/img/music/listSong1/song18.jpg"
+        },
+        {
+            name: "(SHAUN 숀) WAY BACK HOME - Vietnamese cover",
+            singer: ["Hannah Hoang"],
+            path: "./assets/music/listSong1/song19.mp3",
+            image: "./assets/img/music/listSong1/song19.jpg"
+        },
+        {
+            name: "Thêm Một Lần Đau",
+            singer: ["Ngô Lan Hương"],
+            path: "./assets/music/listSong1/song20.mp3",
+            image: "./assets/img/music/listSong1/song20.jpg"
+        },
+        {
+            name: "Bối Rối",
+            singer: ["Đông Nhi"],
+            path: "./assets/music/listSong1/song21.mp3",
+            image: "./assets/img/music/listSong1/song21.jpg"
+        },
+    ],
+    [
+        {
+            name: "Rồi Tới Luôn",
+            singer: ["Nal"],
+            path: "./assets/music/listSong2/song1.mp3",
+            image: "./assets/img/music/listSong2/song1.jpg"
+        },
+        {
+            name: "Yêu Là Cưới",
+            singer: ["PHÁT HỒ X2X"],
+            path: "./assets/music/listSong2/song2.mp3",
+            image: "./assets/img/music/listSong2/song2.jpg"
+        },
+        {
+            name: "Độ Tộc 2",
+            singer: ["Masew", "Độ Mixi", "Phúc Du", "Pháo"],
+            path: "./assets/music/listSong2/song3.mp3",
+            image: "./assets/img/music/listSong2/song3.jpg"
+        },
+        {
+            name: "Chúng Ta Của Hiện Tại",
+            singer: ["Sơn Tùng M-TP"],
+            path: "./assets/music/listSong2/song4.mp3",
+            image: "./assets/img/music/listSong2/song4.jpg"
+        },
+        {
+            name: "Em Không Hiểu",
+            singer: ["Changg", "Minh Huy"],
+            path: "./assets/music/listSong2/song5.mp3",
+            image: "./assets/img/music/listSong2/song5.jpg"
+        },
+        {
+            name: "Đã Lỡ Yêu Em Nhiều",
+            singer: ["JustaTee"],
+            path: "./assets/music/listSong2/song6.mp3",
+            image: "./assets/img/music/listSong2/song6.jpg"
+        },
+        {
+            name: "Cưới Đi",
+            singer: ["2T", "ChangC"],
+            path: "./assets/music/listSong2/song7.mp3",
+            image: "./assets/img/music/listSong2/song7.jpg"
+        },
+        {
+            name: "Ái Nộ",
+            singer: ["Masew", "Khoi Vu"],
+            path: "./assets/music/listSong2/song8.mp3",
+            image: "./assets/img/music/listSong2/song8.jpg"
+        },
+        {
+            name: "Thằng Điên",
+            singer: ["JustaTee", "Phương Ly"],
+            path: "./assets/music/listSong2/song9.mp3",
+            image: "./assets/img/music/listSong2/song9.jpg"
+        },
+        {
+            name: "Yêu Đơn Phương",
+            singer: ["OnlyC", "Karik"],
+            path: "./assets/music/listSong2/song10.mp3",
+            image: "./assets/img/music/listSong2/song10.jpg"
+        },
+        {
+            name: "Trời Giấu Trời Mang Đi",
+            singer: ["AMEE", "VIRUSS"],
+            path: "./assets/music/listSong2/song11.mp3",
+            image: "./assets/img/music/listSong2/song11.jpg"
+        },
+        {
+            name: "Ex's Hate Me",
+            singer: ["B Ray", "Masew", "AMEE"],
+            path: "./assets/music/listSong2/song12.mp3",
+            image: "./assets/img/music/listSong2/song12.jpg"
+        },
+        {
+            name: "The PlayAh(Special Performance / Official Music Video)",
+            singer: ["Soobin", "SlimV"],
+            path: "./assets/music/listSong2/song13.mp3",
+            image: "./assets/img/music/listSong2/song13.jpg"
+        },
+        {
+            name: "Muộn Rồi Mà Sao Còn",
+            singer: ["Sơn Tùng MTP"],
+            path: "./assets/music/listSong2/song14.mp3",
+            image: "./assets/img/music/listSong2/song14.jpg"
+        },
+        {
+            name: "Cưới Thôi",
+            singer: ["Masew", "B Ray", "TAP"],
+            path: "./assets/music/listSong2/song15.mp3",
+            image: "./assets/img/music/listSong2/song15.jpg"
+        },
+        {
+            name: "Mượn Rượu Tỏ Tình",
+            singer: ["Big Daddy", "Emily"],
+            path: "./assets/music/listSong2/song16.mp3",
+            image: "./assets/img/music/listSong2/song16.jpg"
+        },
+        {
+            name: "Yêu Một Người Có Lẽ",
+            singer: ["Lou Hoàng", "Miu Lê"],
+            path: "./assets/music/listSong2/song17.mp3",
+            image: "./assets/img/music/listSong2/song17.jpg"
+        },
+        {
+            name: "Anh Không Đòi Quà",
+            singer: ["OnlyC", "Karik"],
+            path: "./assets/music/listSong2/song18.mp3",
+            image: "./assets/img/music/listSong2/song18.jpg"
+        },
+        {
+            name: "Đi Đu Đưa Đi",
+            singer: ["Bích Phương"],
+            path: "./assets/music/listSong2/song19.mp3",
+            image: "./assets/img/music/listSong2/song19.jpg"
+        },
+        {
+            name: "BlackJack",
+            singer: ["Soobin", "Binz"],
+            path: "./assets/music/listSong2/song20.webm",
+            image: "./assets/img/music/listSong2/song20.jpg"
+        },
+    ],
+    [
+        {
+            name: "Cheating On You",
+            singer: ["Charlie Puth"],
+            path: "./assets/music/listSong3/song1.mp3",
+            image: "./assets/img/music/listSong3/song1.jpg"
+        },
+        {
+            name: "I Do",
+            singer: ["911"],
+            path: "./assets/music/listSong3/song2.mp3",
+            image: "./assets/img/music/listSong3/song2.jpg"
+        },
+        {
+            name: "Reality",
+            singer: ["Lost Frequencies", "Janieck Devy"],
+            path: "./assets/music/listSong3/song3.mp3",
+            image: "./assets/img/music/listSong3/song3.jpg"
+        },
+        {
+            name: "Attention",
+            singer: ["Charlie Puth"],
+            path: "./assets/music/listSong3/song4.mp3",
+            image: "./assets/img/music/listSong3/song4.jpg"
+        },
+        {
+            name: "Monster",
+            singer: ["Katie Sky"],
+            path: "./assets/music/listSong3/song5.mp3",
+            image: "./assets/img/music/listSong3/song5.jpg"
+        },
+    
+        {
+            name: "Faded",
+            singer: ["Alan Walker"],
+            path: "./assets/music/listSong3/song6.mp3",
+            image: "./assets/img/music/listSong3/song6.jpg"
+        },
+        {
+            name: "Save Me",
+            singer: ["DEAMN"],
+            path: "./assets/music/listSong3/song7.mp3",
+            image: "./assets/img/music/listSong3/song7.jpg"
+        },
+        {
+            name: "Havana",
+            singer: ["Camila Cabello", "Young Thug"],
+            path: "./assets/music/listSong3/song8.mp3",
+            image: "./assets/img/music/listSong3/song8.jpg"
+        },
+        {
+            name: "The River",
+            singer: ["Axel Johansson"],
+            path: "./assets/music/listSong3/song9.mp3",
+            image: "./assets/img/music/listSong3/song9.jpg"
+        },
+        {
+            name: "Rendezvous",
+            singer: ["Deamn"],
+            path: "./assets/music/listSong3/song10.mp3",
+            image: "./assets/img/music/listSong3/song10.jpg"
+        },
+        {
+            name: "Way Back",
+            singer: ["Vicetone", "Cozi Zuehlsdorff"],
+            path: "./assets/music/listSong3/song11.mp3",
+            image: "./assets/img/music/listSong3/song11.jpg"
+        },
+         {
+            name: "Peaches",
+            singer: ["Justin Bieber", "Daniel Caesar", "Giveon"],
+            path: "./assets/music/listSong3/song12.mp3",
+            image: "./assets/img/music/listSong3/song12.jpg"
+        },
+        {
+            name: "Stay",
+            singer: ["The Kid LAROI", "Justin Bieber"],
+            path: "./assets/music/listSong3/song13.mp3",
+            image: "./assets/img/music/listSong3/song13.jpg"
+        },
+        {
+            name: "Nevada",
+            singer: ["Vicetone"],
+            path: "./assets/music/listSong3/song14.mp3",
+            image: "./assets/img/music/listSong3/song14.jpg"
+        },
+        {
+            name: "Hero",
+            singer: ["Cash Cash", "Christina Perri"],
+            path: "./assets/music/listSong3/song15.mp3",
+            image: "./assets/img/music/listSong3/song15.jpg"
+        },
+        {
+            name: "Mood",
+            singer: ["24Kgoldn"],
+            path: "./assets/music/listSong3/song16.mp3",
+            image: "./assets/img/music/listSong3/song16.jpg"
+        },
+    ],
+    [
+        {
+            name: "Eyes - Đôi mắt",
+            singer: ["G.Ducky"],
+            path: "./assets/music/listSong4/song1.mp3",
+            image: "./assets/img/music/listSong4/song1.jpg"
+        },
+        {
+            name: "Giàu Vì Bạn Sang Vì Vợ",
+            singer: ["RPT MCK"],
+            path: "./assets/music/listSong4/song2.mp3",
+            image: "./assets/img/music/listSong4/song2.jpg"
+        },
+        {
+            name: "Walk On Da Street",
+            singer: ["16 Typh", "16 BrT"],
+            path: "./assets/music/listSong4/song3.mp3",
+            image: "./assets/img/music/listSong4/song3.jpg"
+        },
+        {
+            name: "CHƠI",
+            singer: ["HIEUTHUHAI", "MANBO"],
+            path: "./assets/music/listSong4/song4.mp3",
+            image: "./assets/img/music/listSong4/song4.jpg"
+        },
+        {
+            name: "Thủ Đô Cypher",
+            singer: ["RPT Orijinn", "LOW G", "RZMas", "RPT MCK"],
+            path: "./assets/music/listSong4/song5.mp3",
+            image: "./assets/img/music/listSong4/song5.jpg"
+        },
+    
+        {
+            name: "Buông Hàng",
+            singer: ["Young Milo"],
+            path: "./assets/music/listSong4/song6.mp3",
+            image: "./assets/img/music/listSong4/song6.jpg"
+        },
+        {
+            name: "Người Chơi Hệ Đẹp",
+            singer: ["16 Typh"],
+            path: "./assets/music/listSong4/song7.mp3",
+            image: "./assets/img/music/listSong4/song7.jpg"
+        },
+        {
+            name: "Ghé Qua",
+            singer: ["Dick ", "PC", "Tofu"],
+            path: "./assets/music/listSong4/song8.mp3",
+            image: "./assets/img/music/listSong4/song8.jpg"
+        },
+        {
+            name: "Đìu Anh Luôn Giữ Kín Trong Tym",
+            singer: ["RPT MCK", "tlinh", "2pillz "],
+            path: "./assets/music/listSong4/song9.mp3",
+            image: "./assets/img/music/listSong4/song9.jpg"
+        },
+        {
+            name: "Xích Thêm Chút",
+            singer: ["RPT Groovie", "TLinh", "RPT MCK"],
+            path: "./assets/music/listSong4/song10.mp3",
+            image: "./assets/img/music/listSong4/song10.jpg"
+        },
+        {
+            name: "Tay To",
+            singer: ["RPT MCK", "RPT PhongKhin"],
+            path: "./assets/music/listSong4/song11.mp3",
+            image: "./assets/img/music/listSong4/song11.jpg"
+        },
+        {
+            name: "SIMPLE LOVE",
+            singer: ["Obito", "Seachains", "Davis", "Lena"],
+            path: "./assets/music/listSong4/song12.mp3",
+            image: "./assets/img/music/listSong4/song12.jpg"
+        },
+        {
+            name: "Va Vào Giai Điệu Này",
+            singer: ["RPT MCK"],
+            path: "./assets/music/listSong4/song13.mp3",
+            image: "./assets/img/music/listSong4/song13.jpg"
+        },
+         {
+            name: "Em Gái",
+            singer: ["Blaise", "Low G"],
+            path: "./assets/music/listSong4/song14.mp3",
+            image: "./assets/img/music/listSong4/song14.jpg"
+        },
+        {
+            name: "OK",
+            singer: ["Binz"],
+            path: "./assets/music/listSong4/song15.mp3",
+            image: "./assets/img/music/listSong4/song15.jpg"
+        },
+        {
+            name: "Gene",
+            singer: ["Binz"],
+            path: "./assets/music/listSong4/song16.mp3",
+            image: "./assets/img/music/listSong4/song16.jpg"
+        },
+         {
+            name: "They Said",
+            singer: ["Binz"],
+            path: "./assets/music/listSong4/song17.mp3",
+            image: "./assets/img/music/listSong4/song17.jpg"
+        },
+    ],
+];
+
+const MUSIC_STORAGE_KEY = 'VIK_MUSIC';
+
+localStorage.setItem(MUSIC_STORAGE_KEY, JSON.stringify(listSong));
+
+
